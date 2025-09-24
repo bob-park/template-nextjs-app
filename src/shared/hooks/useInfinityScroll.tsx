897306f7560a0ad53158658a339766d510e03ac9
Bob Park <hwpark11@kakao.com>
2025-09-24 23:19:52 +0900

    refactor: use infinity scroll 에 option 추가

--- src/shared/hooks/useInfinityScroll.tsx
+++ src/shared/hooks/useInfinityScroll.tsx
@@ -2,10 +2,20 @@
 
 import { useEffect, useRef } from 'react';
 
+interface InfinityScrollProps {
+  hasMore: boolean;
+  options?: {
+    threshold: number;
+    intersectionRatio: number;
+  };
+  onNext?: () => Promise<void>;
+}
+
 export default function useInfinityScroll({
   hasMore,
+  options = { threshold: 1, intersectionRatio: 1 },
   onNext,
-}: Readonly<{ hasMore: boolean; onNext?: () => Promise<void> }>) {
+}: Readonly<InfinityScrollProps>) {
   // ref
   const ref = useRef<HTMLDivElement>(null);
 
@@ -17,13 +27,13 @@ export default function useInfinityScroll({
 
     const observer = new IntersectionObserver(
       async (entries) => {
-        const isExecute = entries.every((entry) => entry.intersectionRatio === 1);
+        const isExecute = entries.every((entry) => entry.intersectionRatio > options?.intersectionRatio);
 
         if (isExecute) {
           await onNext?.();
         }
       },
-      { threshold: 1 },
+      { threshold: options.threshold },
     );
 
     hasMore ? observer.observe(ref.current) : observer.unobserve(ref.current);
