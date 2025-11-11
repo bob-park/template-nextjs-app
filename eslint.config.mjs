58da89696ef23a03e1303d25cebd986c9ea1ec2b
Bob Park <hwpark@malgn.com>
2025-11-11 15:42:14 +0900

    build: eslint next 설정 분리 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
 // eslint.config.mjs
 import eslintConfig from '@bob-park/eslint-config-bobpark';
 
+import { FlatCompat } from '@eslint/eslintrc';
 import { defineConfig } from 'eslint/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default defineConfig([
-    {
-        extends: [eslintConfig],
-    },
+  {
+    extends: [eslintConfig],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 ]);
--- src/shared/store/rootStore.ts
+++ src/shared/store/rootStore.ts
@@ -1,18 +1,16 @@
-import createDeviceSlice from '@/domain/devices/store/slice';
 import createUserSlice from '@/domain/users/store/slice';
 
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 export const useStore = create<BoundState>()(
   devtools(
     immer((...a) => ({
       ...createUserSlice(...a),
-      ...createDeviceSlice(...a),
     })),
     { name: 'smart-scoreboard-manager', enabled: process.env.NODE_ENV !== 'production' },
   ),
 );
 
-export type BoundState = UserState & DeviceState;
+export type BoundState = UserState;
