38d5408e70a4a3861388990c9a9d884c9c2c25c3
Bob Park <hwpark11@kakao.com>
2026-08-05 01:41:21 +0900

    feat: server component get session 예시 추가

--- src/app/_layouts/Header.tsx
+++ src/app/_layouts/Header.tsx
@@ -1,5 +1,6 @@
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 
+import { auth } from '@/shared/auth';
 import LanguageSwitcher from '@/shared/components/i18n/LanguageSwitcher';
 import ThemeSwitcher from '@/shared/components/theme/ThemeSwitcher';
 import { Theme } from '@/shared/providers/theme/ThemeProvider';
@@ -10,9 +11,21 @@ export default async function Header() {
   const cookieStore = await cookies();
   const theme = (cookieStore.get(COOKIE_NAME_THEME)?.value ?? 'light') as Theme;
 
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <header className="flex w-full flex-row items-center justify-between">
-      <div className="">header</div>
+      <div className="">
+        username: <span>{session?.user.name}</span>
+      </div>
+
+      <div>
+        <a className="btn" href="/logout">
+          logout
+        </a>
+      </div>
 
       <div className="">
         <LanguageSwitcher />
