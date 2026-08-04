02844685e115ce43b2dc6607ee5fc01a712c9ee5
Bob Park <hwpark11@kakao.com>
2026-08-05 01:28:00 +0900

    feat: toast 메세지 예시 추가

--- src/app/_layouts/Header.tsx
+++ src/app/_layouts/Header.tsx
@@ -1,12 +1,25 @@
-import LanguageSwitcher from '@/shared/components/i18n/LanguageSwitcher';
+import { cookies } from 'next/headers';
 
-export default function Header() {
+import LanguageSwitcher from '@/shared/components/i18n/LanguageSwitcher';
+import ThemeSwitcher from '@/shared/components/theme/ThemeSwitcher';
+import { Theme } from '@/shared/providers/theme/ThemeProvider';
+
+const COOKIE_NAME_THEME = 'theme';
+
+export default async function Header() {
+  const cookieStore = await cookies();
+  const theme = (cookieStore.get(COOKIE_NAME_THEME)?.value ?? 'light') as Theme;
+
   return (
     <header className="flex w-full flex-row items-center justify-between">
       <div className="">header</div>
 
       <div className="">
         <LanguageSwitcher />
+      </div>
+
+      <div className="">
+        <ThemeSwitcher current={theme} />
       </div>
     </header>
   );
