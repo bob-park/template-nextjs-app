227879aff4fa32965f8ab47467e07712fe432074
Bob Park <hwpark11@kakao.com>
2026-08-21 01:01:37 +0900

    fix: theme cookie options 추가

--- src/app/themeAction.ts
+++ src/app/themeAction.ts
@@ -9,5 +9,9 @@ const COOKIE_NAME_THEME = 'theme';
 export async function setTheme(theme: Theme) {
   const cookieStore = await cookies();
 
-  cookieStore.set(COOKIE_NAME_THEME, theme);
+  cookieStore.set(COOKIE_NAME_THEME, theme, {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 365, // 365 days
+    sameSite: 'lax',
+  });
 }
